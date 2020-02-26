caa0b912c6963847ff062bd385cf7151fed4a54c
Khamit <khamitamantaev@gmail.com>
2020-02-27 00:57:02 +0500

    контроллеры

---
+++ demo/src/main/java/com/vgrupper/demo/controllers/ProductController.java
@@ -0,0 +1,27 @@
+package com.vgrupper.demo.controllers;
+
+import java.util.Optional;
+
+import com.vgrupper.demo.entity.Products;
+import com.vgrupper.demo.repositories.ProductRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin(origins = "*")
+@RequestMapping("/api")
+@RestController
+public class ProductController {
+
+    @Autowired
+    ProductRepository productRepository;
+
+    @RequestMapping(method = RequestMethod.GET, value = "/products")
+    public Iterable<Products> product() {
+        return productRepository.findAll();
+    }
+}
