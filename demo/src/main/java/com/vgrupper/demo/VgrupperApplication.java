d2e3ad3c15d04097ff28b388f949ee3635ced17f
Khamit <khamitamantaev@gmail.com>
2020-02-26 23:51:12 +0500

    загрузил проект стартовый

---
+++ demo/src/main/java/com/vgrupper/demo/VgrupperApplication.java
@@ -0,0 +1,13 @@
+package com.vgrupper.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VgrupperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VgrupperApplication.class, args);
+	}
+
+}
