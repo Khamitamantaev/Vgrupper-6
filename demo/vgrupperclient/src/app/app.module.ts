1ce2fb4b64cbdcfdff486f3d278f8de925d32e73
Khamit <khamitamantaev@gmail.com>
2020-02-27 01:22:18 +0500

    добавил компоненты angular в проект

--- demo/vgrupperclient/src/app/app.module.ts
+++ demo/vgrupperclient/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ProductsComponent } from './products/products.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ProductsComponent,
+    LoginComponent,
+    RegisterComponent
   ],
   imports: [
     BrowserModule
